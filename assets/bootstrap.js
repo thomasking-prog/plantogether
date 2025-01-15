4c6a4f4553457d68650a2c341b114242bcffe469
thomasking-prog <thomasking.prog@gmail.com>
2025-01-15 08:29:37 +0100

    initial commit

---
+++ assets/bootstrap.js
@@ -0,0 +1,6 @@
+import { startStimulusApp } from '@symfony/stimulus-bundle';
+
+startStimulusApp();
+//const app = startStimulusApp();
+// register any custom, 3rd party controllers here
+// app.register('some_controller_name', SomeImportedController);
