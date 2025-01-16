030e87647c8ee09b68e8880b077c3b4a76029416
thomasking-prog <thomasking.prog@gmail.com>
2025-01-16 15:05:31 +0100

    user + login & registration

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
